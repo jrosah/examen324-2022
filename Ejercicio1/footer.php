ab9c09cf1be710228bbcf63220d30972aa75f24c
jrosah <84873519+jrosah@users.noreply.github.com>
2022-10-09 15:23:12 -0400

    Add files via upload

--- Ejercicio1/footer.php
+++ Ejercicio1/footer.php
@@ -1,7 +1,7 @@
 
       <!-- FOOTER -->
       <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Jorge Ariel Rrosa Humiri 2022</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Jorge Ariel Rosa Humiri 2022</p></div>
         </footer>
     </main>
 
